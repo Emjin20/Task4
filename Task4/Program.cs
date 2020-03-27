1723b0a47d79d2c7a5d6f4e214363b5d35c81892
Emjin20 <62754587+Emjin20@users.noreply.github.com>
2020-03-27 19:27:19 +0400

    Добавьте файлы проекта.

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fReady = 1;
+            int factorFinish = Factorial(fReady);
+        }
+        static int Factorial(int fReady)
+        {
+            int factorFinish = 0;
+            string number;
+            Console.WriteLine("Введите число n");
+            number = Console.ReadLine();
+            int number1 = Convert.ToInt32(number);
+
+            for (int i = 0; i< number1; i++)
+            {
+                fReady = fReady * (i + 1);
+            }
+            Console.WriteLine(fReady);
+            return factorFinish;
+        }
+    }
+}
